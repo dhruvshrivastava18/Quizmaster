5df9adb1df9a97262704bb7cc354a7486a112c3b
DHRUV SHRIVASTAVA <dhruvshrivastava18@gmail.com>
2022-10-02 10:08:52 +0530

    Ui Improvement

---
+++ Quizmaster/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizmaster
+//
+//  Created by Dhruv Shrivastava on 28/08/22.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
